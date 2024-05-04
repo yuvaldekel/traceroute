e551017af14446b405fdc427827e2648c4db6027
yuvallinux <yuvddd05@gmail.com>
2024-05-04 17:51:00 +0300

    stop at the last stop

--- traceroute.py
+++ traceroute.py
@@ -1,4 +1,4 @@
-from scapy.all import IP, ICMP, Raw, sr1
+from scapy.all import IP, ICMP, Raw, sr1, Net
 import datetime
 
 
@@ -15,8 +15,8 @@ def alphabet_list(start = "A", end = 'z'):
     return ''.join(alphabet)
 
 
-def send_ttl(num, data):
-    trace_packet = IP(dst = "www.google.com", ttl = num)/ICMP(type = 8, code = 0, id = num, seq = num)/Raw(data)
+def send_ttl(dst_ip, num, data):
+    trace_packet = IP(dst = dst_ip, ttl = num)/ICMP(type = 8, code = 0, id = num, seq = num)/Raw(data)
     start = datetime.datetime.now()
     answer = sr1(trace_packet, timeout =1)
     end = datetime.datetime.now()
@@ -24,16 +24,24 @@ def send_ttl(num, data):
     return answer, timing
 
 def main():
+    dst_ip = input("Enter destination: ")
+    
     alphabet = alphabet_list(end= 'z')
+    
+    n = 0
+    while True:
 
-    for n in range(1,5):
-        answer, timimg = send_ttl(n, alphabet)
+        answer, timimg = send_ttl(dst_ip, n, alphabet)
         
         if answer == None:
             print(f"Router {n} did not answer after {timimg.total_seconds():.3f} seconds.")
         else:
             n_ip = answer[IP].src
             print(f"Router {n} ip is {n_ip} took {timimg.total_seconds():.3f} seconds to get an answer.")
+        n +=1
+        
+        if answer != None and answer[IP].src == Net(dst_ip):
+            break
 
 if __name__ == "__main__":
     main()
